f18a6d7ce6c1e83b6bb2b3ab11c01c6225c5dfda
edwinjs <edwinjs31@gmail.com>
2022-04-14 12:05:44 +0200

    Modificacion de estilos en la clase 'alerta'

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,5 +1,5 @@
-<h1 class="nombre-pagina">Login</h1>
-<p class="descripcion-pagina">Inicia sesión con tus datos</p>
+<h1 class="nombre-pagina">Iniciar Sesión</h1>
+<p class="descripcion-pagina">Bienvenido</p>
 
 <?php 
     include_once __DIR__ . "/../templates/alertas.php";
@@ -30,6 +30,6 @@
 </form>
 
 <div class="acciones">
-    <a href="/crear-cuenta">¿Aún no tienes una cuenta? Crear una</a>
+    <a href="/crear-cuenta">¿Aún no tienes cuenta? <span>Regístrate ahora</span></a>
     <a href="/olvide">¿Olvidaste tu password?</a>
 </div>
