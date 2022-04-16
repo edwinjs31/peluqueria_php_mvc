f1a8db59aca4aa432d7502d408c09d24d5628c2d
edwinjs <edwinjs31@gmail.com>
2022-04-16 12:18:06 +0200

    Vista del admin , correccion de servicios null

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,7 +1,7 @@
 <h1 class="nombre-pagina">Panel de Administración</h1>
 
-<?php 
-    include_once __DIR__ . '/../templates/barra.php';
+<?php
+include_once __DIR__ . '/../templates/barra.php';
 ?>
 
 <h2>Buscar Citas</h2>
@@ -9,32 +9,27 @@
     <form class="formulario">
         <div class="campo">
             <label for="fecha">Fecha</label>
-            <input 
-                type="date"
-                id="fecha"
-                name="fecha"
-                value="<?php echo $fecha; ?>"
-            />
+            <input type="date" id="fecha" name="fecha" value="<?php echo $fecha; ?>" />
         </div>
-    </form> 
+    </form>
 </div>
 
 <?php
-    if(count($citas) === 0) {
-        echo "<h2>No Hay Citas en esta fecha</h2>";
-    }
+if (count($citas) === 0) {
+    echo "<h2>No Hay Citas en esta fecha</h2>";
+}
 ?>
 
 <div id="citas-admin">
-    <ul class="citas">   
-            <?php 
-                $idCita = 0;
-                foreach( $citas as $key => $cita ) {
-   
-                    if($idCita !== $cita->id) {
-                        $total = 0;
-            ?>
-            <li>
+    <ul class="citas">
+        <?php
+        $idCita = 0;
+        foreach ($citas as $key => $cita) {
+
+            if ($idCita !== $cita->id) {
+                $total = 0;
+        ?>
+                <li>
                     <p>ID: <span><?php echo $cita->id; ?></span></p>
                     <p>Hora: <span><?php echo $cita->hora; ?></span></p>
                     <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
@@ -42,18 +37,25 @@
                     <p>Teléfono: <span><?php echo $cita->telefono; ?></span></p>
 
                     <h3>Servicios</h3>
-            <?php 
+                <?php
                 $idCita = $cita->id;
             } // Fin de IF 
-                $total += $cita->precio;
-            ?>
-                    <p class="servicio"><?php echo "- ". $cita->servicio . ":  " . $cita->precio."€"; ?></p>
             
-            <?php 
-                $actual = $cita->id;
-                $proximo = $citas[$key + 1]->id ?? 0;
+            $total += $cita->precio;
+            $servicio = $cita->servicio;
 
-                if(esUltimo($actual, $proximo)) { ?>
+            if ($servicio !== null) {
+
+                ?>
+                    <p class="servicio"><?php echo "- " . $servicio . ":  " . $cita->precio . "€"; ?></p>
+
+                <?php
+            }
+
+            $actual = $cita->id;
+            $proximo = $citas[$key + 1]->id ?? 0;
+
+            if (esUltimo($actual, $proximo)) { ?>
                     <p class="total">Total: <span> <?php echo $total; ?>€</span></p>
 
                     <form action="/api/eliminar" method="POST">
@@ -61,11 +63,12 @@
                         <input type="submit" class="boton-eliminar" value="Eliminar">
                     </form>
 
-            <?php } 
-          } // Fin de Foreach ?>
-     </ul>
+            <?php }
+        } // Fin de Foreach 
+            ?>
+    </ul>
 </div>
 
 <?php
-    $script = "<script src='build/js/buscador.js'></script>"
+$script = "<script src='build/js/buscador.js'></script>"
 ?>
