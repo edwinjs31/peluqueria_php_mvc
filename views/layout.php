da10490da8d0e51a2615da5b48c81131a182e87f
edwinjs <edwinjs31@gmail.com>
2022-05-14 12:44:23 +0200

    Agregado el navbar

--- views/layout.php
+++ views/layout.php
@@ -7,6 +7,7 @@
     <title>App Salón</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/build/css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 
 <body>
@@ -14,10 +15,9 @@
         <!-- imagen de fondo y el navBar -->
         <div class="imagen">
             <div class="navBar">
-                <a class="active" href="/">Home</a>
+                <a class="active" href="/"><i class="fa-solid fa-house"></i></a>
                 <a href="/contacto">Contacto</a>
                 <a href="/nosotros">Sobre nosotros</a>
-                <a href="/">Login</a>
             </div>
         </div>
         <!-- contenido y funcionalidad de la app -->
--- views/templates/contacto.php
+++ views/templates/contacto.php
@@ -1,2 +1,40 @@
-<h1 class="nombre-pagina">Contactos</h1>
-<p class="descripcion-pagina">Formulario de contacto</p>
+
+<h1 class="nombre-pagina">Contacto</h1>
+
+<div class="info-empresa">
+    <h3>AppSalon</h3>
+    <ul class="servicios">
+        <li><i class="fa fa-map-marker"></i> Calle Constitución </li>
+        <li><i class="fa fa-mobile"></i> 555 555 000</li>
+        <li><i class="fa fa-envelope"></i> info@empresa.com</li>
+    </ul>
+</div>
+
+<?php
+include_once __DIR__ . "/../templates/alertas.php";
+?>
+
+<form class="formulario" method="POST" action="">
+
+    <div class="campo">
+        <label for="nombre">Nombre</label>
+        <input type="text" id="nombre" name="nombre" placeholder="Tu Nombre" value="" />
+    </div>
+
+
+    <div class="campo">
+        <label for="email">E-mail</label>
+        <input type="email" id="email" name="email" placeholder="Tu E-mail" value="" />
+    </div>
+
+    <div class="campo">
+        <label for="mensaje">Escribe tu mensaje aquí:</label>
+        <textarea name="mensaje" id="mensaje" cols="30" rows="10"></textarea>
+
+    </div>
+
+
+    <input type="submit" value="Enviar" class="boton">
+
+
+</form>
