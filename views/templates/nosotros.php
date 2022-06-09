e0f838f0fcd0b80e47efdf1a93b49c878fe43644
edwinjs <edwinjs31@gmail.com>
2022-06-09 19:51:18 +0200

    Apartado nosotros terminado

--- views/templates/nosotros.php
+++ views/templates/nosotros.php
@@ -4,62 +4,98 @@
 </head>
 
 <body>
-    <h1>NOSOTROS</h1>
+    <h1>SOBRE NOSOTROS</h1>
     <p class="texto-nosotros">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Accusamus, aliquam, asperiores, atque, autem, consequuntur
-        corporis cumque debitis doloremque dolores dolorum eaque earum
-        eligendi, enim error esse et eum ex explicabo facere fugiat
-        fugit harum illo impedit in inventore ipsa ipsum iste itaque
-        iure laboriosam laborum laudantium libero magni maxime minima
-        minus modi molestiae nam natus necessitatibus nemo neque nihil.
+        La <span class="nombre-salon">Blue Velvet</span> es una peluquería masculina caracterizada por su estilo moderno, especializada en corte de pelo y color, afeitado con paños calientes y mascarilla para un restablecimiento de la piel óptimo. Además, sus peluqueros adaptan su técnica a todo tipo de tendencias.<br>
+        Abrió sus puertas en el año 2000 en la Av. Constitución 14, Abarán Murcia, con un solo peluquero.
+        Hoy en día la <span class="nombre-salon">Blue Velvet</span> cuenta con una red de peluqueros que se extiende a toda la provincia de Murcia.
     </p>
-    <!-- <h1 class="nombre-pagina">Sobre nosotros</h1>
-<p class="descripcion-pagina">imágenes y cosas sobre nosotros</p> -->
-    <!-- <div class="row">
-        <div class="main">
-            <h2>TITLE HEADING</h2>
-            <h5>Title description, Dec 7, 2017</h5>
-            <div class="fakeimg"></div>
-            <p>Some text..</p>
-            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-            <br>
-            <h2>TITLE HEADING</h2>
-            <h5>Title description, Sep 2, 2017</h5>
-            <div class="fakeimg"></div>
-            <p>Some text..</p>
-            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-        </div>
-    </div> -->
+    <hr class="separador">
     <div class="row">
         <div class="main">
-            <h2>TITLE HEADING</h2>
-            <h5>Title description, Dec 7, 2017</h5>
+            <h2>Corte y color</h2>
+            <!-- <h5>Title description, Dec 7, 2017</h5> -->
+            <p class="texto-nosotros">Nuestros estilistas te ayudarán a definir el mejor look. Ritual lavado, corte y styling. Duración: 35-40 min.</p>
             <div class="image-container">
-                <div class="fakeimg" style="background-image: url(../build/img/imagen20.jpg)"></div>
                 <div class="fakeimg" style="background-image: url(../build/img/imagen21.jpg)"></div>
+                <div class="fakeimg" style="background-image: url(../build/img/imagen26.jpg)"></div>
             </div>
-            <p>Some text..</p>
-            <p class="texto-nosotros">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-            <br>
+            <p class="texto-nosotros">Servicio de coloración con efecto fresco y natural que respeta el bienestar del cuero cabelludo. Duración: 35 min.</p>
+            <div class="image-container">
+                <div class="fakeimg" style="background-image: url(../build/img/imagen36.jpg)"></div>
+                <div class="fakeimg" style="background-image: url(../build/img/imagen35.jpg)"></div>
+            </div>
+            <p class="texto-nosotros">Si eres de los que le gusta seguir las tendencias y estar a la última, te asesoramos para que escojas el color más apropiado con tu estilo de vida y personalidad.
+                Duración: 35 min.</p>
+            <div class="image-container">
+                <div class="fakeimg" style="background-image: url(../build/img/imagen32.jpg)"></div>
+                <div class="fakeimg" style="background-image: url(../build/img/imagen37.jpg)"></div>
+            </div>
         </div>
     </div>
+    <hr class="separador">
     <div class="row">
         <div class="main">
-            <h2>TITLE HEADING</h2>
-            <h5>Title description, Dec 7, 2017</h5>
+            <h2>Barbería: estilismo y afeitado</h2>
+            <p class="texto-nosotros">Una barba cuidada aporta siempre una imagen pulcra y elegante, para aquellos que son fieles a su personalidad. Duración: 20 min.</p>
             <div class="image-container">
-                <div class="fakeimg" style="background-image: url(../build/img/imagen22.jpg)"></div>
-                <div class="fakeimg" style="background-image: url(../build/img/imagen23.jpg)"></div>
+                <div class="fakeimg" style="background-image: url(../build/img/imagen1.jpg)"></div>
+                <div class="fakeimg" style="background-image: url(../build/img/imagen20.jpg)"></div>
             </div>
-            <p>Some text..</p>
-            <p class="texto-nosotros">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-            <br>
+            <p class="texto-nosotros">Consigue una piel 100% apurado y 100% suave. Incluye sérum pre afeitado, afeitado, after shave hidratante con masaje. Duración: 30 min.</p>
+            <div class="image-container">
+                <div class="fakeimg" style="background-image: url(../build/img/imagen33.jpg)"></div>
+                <div class="fakeimg" style="background-image: url(../build/img/imagen34.jpg)"></div>
+            </div>
         </div>
     </div>
-
+    <hr class="separador">
+    <div class="row">
+        <h1>QUE OFRECEMOS</h1>
+        <ul class="lista_servicios">
+            <li>Corte de pelo máquina -- 16,00</li>
+            <li>Corte de pelo tijera, lavado y secado -- 17,50</li>
+            <li>Corte de pelo navaja -- 23,00</li>
+            <li>Lavar, peinar y secar -- 10,00</li>
+            <li>Arreglo de barba -- 12,50</li>
+            <li>Arreglo de bigote con afeitado -- 6,50</li>
+            <li>Arreglo de barba con afeitado -- 17,50</li>
+            <li>Afeitado con masaje -- 17,50</li>
+            <li>Afeitado con mascarilla y masaje -- 30,00</li>
+            <li>Quemado de puntas -- 6,00</li>
+            <li>Tinte, matizador o color -- 19,00</li>
+            <li>Servicio a domicilio -- 40,00</li>
+        </ul>
+    </div>
+    <hr class="separador">
+    <div class="citas-info">
+        <h1>CITAS</h1>
+        <p class="texto-nosotros">
+            Para reservar una cita, regístrate o llama al teléfono <span class="telefono-salon">958 060 060</span> o escribe a nuestro correo electrónico <a href="/contacto">aquí</a> y le responderemos a la brevedad.
+        </p>
+    </div>
     <div class="footer">
-        <h2>Footer</h2>
+        <div class="contacto">
+            <h1 class="titulo-footer">CONTACTO</h1>
+            <p>
+            <p><span class="nombre-salon">Blue Velvet</span></p>
+            <p><i class="fa fa-map-marker"></i> Av. Constitución 14</p>
+            <p>Abarán, Murcia</p>
+            <p>30550</p>
+            <p><i class="fa fa-mobile"></i> Teléfono: 961 664 030</p>
+            <p><i class="fa fa-envelope"></i> Email: <a target="blank" href="mailto:
+                bluevelvet@gmail.com">bluevelvet@gmail.com</a></p>
+
+            </p>
+        </div>
+        <div class="redes">
+            <h1 class="titulo-footer">SIGUENOS</h1>
+            <a target="blank" href="https://www.facebook.com/"><i class="fa-brands fa-facebook"></i></a>
+            <a target="blank" href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a>
+            <a target="blank" href="https://www.twitter.com/"><i class="fa-brands fa-twitter"></i></a>
+            <p>Peluqueria BlueVelvet &copy; 2022</p>
+        </div>
+
     </div>
 
 </body>
