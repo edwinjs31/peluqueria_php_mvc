df88b37aee1af53ad6948b6efe32357fe31f87d4
edwinjs <edwinjs31@gmail.com>
2022-06-12 18:08:49 +0200

    Solucion al problema de rutas

--- Router.php
+++ Router.php
@@ -7,37 +7,45 @@ class Router
     public array $getRoutes = [];
     public array $postRoutes = [];
 
-    public function get($url, $fn) {
+    public function get($url, $fn)
+    {
         $this->getRoutes[$url] = $fn;
     }
 
-    public function post($url, $fn) {
+    public function post($url, $fn)
+    {
         $this->postRoutes[$url] = $fn;
     }
 
-    public function comprobarRutas() {
-         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+    public function comprobarRutas()
+    {
+        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         // $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
-        
+        //$currentUrl = strtok($_SERVER["REQUEST_URI"], '?') ?? '/';
+
         $method = $_SERVER['REQUEST_METHOD'];
 
+        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
+        $splitURL = explode('?', $currentUrl);
+
         if ($method === 'GET') {
-            $fn = $this->getRoutes[$currentUrl] ?? null;
+            $fn = $this->getRoutes[$splitURL[0]] ?? null;//$splitURL[0] contiene la URL
         } else {
-            $fn = $this->postRoutes[$currentUrl] ?? null;
+            $fn = $this->postRoutes[$splitURL[0]] ?? null;
         }
 
-        if ( $fn ) {
+        if ($fn) {
             // Call user fn va a llamar una función cuando no sabemos cual sera
             call_user_func($fn, $this); // This es para pasar argumentos
         } else {
-             echo "Página No Encontrada o Ruta no válida";
+            echo "Página No Encontrada o Ruta no válida";
             //cuando no se encuentre la ruta lo redirigimos a la ruta por defecto(/)
             //header('Location: /');
         }
     }
 
-    public function render($view, $datos = []) {
+    public function render($view, $datos = [])
+    {
         // Leer lo que le pasamos  a la vista
         foreach ($datos as $key => $value) {
             $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
